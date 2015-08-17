3af7f98bb45bd1e95bde04bc329f377cc6236a32
chinhle94115 <chinhle@aol.com>
2015-08-17 13:20:49 -0700

    lesson_five notes

--- 05_Ruby_Fundamentals/exercises/get_mashable.rb
+++ 05_Ruby_Fundamentals/exercises/get_mashable.rb
@@ -13,31 +13,34 @@
 require 'rest-client'
 require 'pry'
 require 'json'
+require 'csv'
 
 def connect_to_api(url)       #this is a starter kit, you need to revise
   response = RestClient.get(url)      #get info from website
   parsed = JSON.parse(response)       #turns info into a hash, and makes it readable
 end
 
-def find_stories(response)
-  stories = response["data"]["children"]
-  puts "The internet has blessed us with #{stories.count} stories."
+def find_stories(parsed)
+  stories = parsed["hot"]
+  puts "There are #{stories.count} 'hot' stories at mashable."
   return stories  #need to return this so that it can be used by other methods
 end
 
 def print_stories(stories)
   stories.each do |story|
-    puts create_story_hash(story["data"]) #by having (story["data"]), now has "data" root
+    puts create_story_hash(story)
+    #if you want the story content, I would add 'content: story["content"]["plain"]' to the new story method below
   end
 end
 
 def create_story_hash(story)
-  new_story = {category: story["subreddit"], title: story["title"], upvotes: story["ups"]}
-  #["data"] not needed because we made it a root above
+  new_story = {category: story["channel"], title: story["title"], upvotes: story["shares"]["total"]}
 end
 
 
-reddit_url = "http://www.reddit.com/.json"
-reddit_json_response = connect_to_api(reddit_url)
-stories = find_stories(reddit_json_response)
+
+
+mashable_url = "http://mashable.com/stories.json"
+mashable_json_response = connect_to_api(mashable_url)
+stories = find_stories(mashable_json_response)
 print_stories(stories)
